2d8d04abeea529532dc8925a9b2d7bb9b26e3f61
imacaveman <153271834+imacaveman@users.noreply.github.com>
2024-02-12 15:20:26 +0000

    added light mode for a laugh

--- js/myjs.js
+++ js/myjs.js
@@ -12,17 +12,30 @@ function displayImage() {
 
 displayImage();
 
-function assignImage(emailAdd, imgSource){
-    $assignedPair.push({email: emailAdd, imgLink: imgSource})
-    console.log("Added to array: ", {email: emailAdd, imgLink: imgSource});
-    console.log("Updated array: ", $assignedPair);
+function assignImage(emailAdd, imgSource) {
+    const existingPairIndex = $assignedPair.findIndex(pair => pair.email === emailAdd);
+    if (existingPairIndex !== -1) {
+        const existingImageIndex = $assignedPair[existingPairIndex].imgLink.indexOf(imgSource);
+        if (existingImageIndex === -1) {
+            $assignedPair[existingPairIndex].imgLink.push(imgSource);
+        } else {
+            console.log("Testing");
+        }
+    } else {
+        $assignedPair.push({ email: emailAdd, imgLink: [imgSource] });
+    }
 }
 
 function applyGlowEffect(buttonSelector, targetSelector) {
     $(buttonSelector).on("click", function(){
-        $(targetSelector).addClass("focus-glow");
+        if ($("body").hasClass("lightmode")) {
+            $(targetSelector).addClass("focus-glow-light");
+        } else {
+            $(targetSelector).addClass("focus-glow");
+        }
         setTimeout(function(){
             $(targetSelector).removeClass("focus-glow");
+            $(targetSelector).removeClass("focus-glow-light");
         }, 2000);
     });
 }
@@ -43,17 +56,30 @@ $("#email-btn").on("click", function(){
     if($regex.test($emailIn)) {
         if($assignedPair.some(pair => pair.email === $emailIn)) {
             $(".input-error-glow").addClass("email-error-glow");
-            $(".error-msg-input").css("color", "#ff5858");
+            if($("body").hasClass("lightmode")) {
+                $(".error-msg-input").css("color", "#ff0000");
+                $(".input-error-glow").addClass("light-error");
+            } else {
+                $(".error-msg-input").css("color", "#ff5858");
+            }
             $(".error-msg-input").text("Email address already exists").show();
             setTimeout(function(){
                 $(".error-msg-input").fadeOut(500);
             }, 3000);
         } else {
             assignImage($emailIn, $currentImg);
+            var $trimEmail = $emailIn.replace(/[@.]/g, "");
             $("#select-email").append("<option>" + $emailIn + "</option>");
-            $(".error-msg-input").css("color", "#2fca00");
+            $("#select-email option").last().prop("selected", true);
+            $("#select-email").change();
+            if($("body").hasClass("lightmode")) {
+                $(".error-msg-input").css("color", "#259e01");
+            } else {
+                $(".error-msg-input").css("color", "#2fca00");
+            }
             $(".error-msg-input").text("Email address added!").show();
-            $("")
+            $(`<ul class="img-storage-${$trimEmail}"></ul>`).appendTo(".assigned-image-box");
+            $(`.img-storage-${$trimEmail}`).append(`<li><img src=${$currentImg} alt=${$emailIn}></li>`);
             setTimeout(function(){
                 $(".error-msg-input").fadeOut(500);
             }, 3000);
@@ -61,7 +87,12 @@ $("#email-btn").on("click", function(){
         $("#email-input").val("");
     } else {
         $(".input-error-glow").addClass("email-error-glow");
-        $(".error-msg-input").css("color", "#ff5858");
+        if($("body").hasClass("lightmode")) {
+            $(".error-msg-input").css("color", "#ff0000");
+            $(".input-error-glow").addClass("light-error");
+        } else {
+            $(".error-msg-input").css("color", "#ff5858");
+        }
         $(".error-msg-input").text("Invalid email address").show();
         $("#email-input").val("");
         setTimeout(function(){
@@ -72,24 +103,96 @@ $("#email-btn").on("click", function(){
 
 $(".input-error-glow").on("click", function(){
     $(this).removeClass("email-error-glow");
+    $(this).removeClass("light-error");
 });
 
 // EMAIL SELECT 
 
 $("#select-btn").on("click", function(){
-
     let $selectValue = $("#select-email")[0].options[$("#select-email")[0].selectedIndex].value;
     if ($selectValue === "Select an Email Address") {
         $(".select-error-glow").addClass("email-error-glow");
-        $(".error-msg-select").css("color", "#ff5858");
-        $(".error-msg-select").text("Please select an email address");
+        if($("body").hasClass("lightmode")) {
+            $(".error-msg-select").css("color", "#ff0000");
+            $(".select-error-glow").addClass("light-error");
+        } else {
+            $(".error-msg-select").css("color", "#ff5858");
+        }
+        $(".error-msg-select").text("Please select an email address").show();
+        setTimeout(function(){
+            $(".error-msg-select").fadeOut(500);
+        }, 3000);
     } else {
-        assignImage($selectValue, $currentImg);
-        console.log($assignedPair);
-        $(`.img-storage`).append(`<li><img src=${$currentImg} alt=${$selectValue}></li>`);
-    } 
+        let existingPairIndex = $assignedPair.findIndex(pair => pair.email === $selectValue);
+        if (existingPairIndex !== -1 && $assignedPair[existingPairIndex].imgLink.includes($currentImg)) {
+            $(".select-error-glow").addClass("email-error-glow");
+            if($("body").hasClass("lightmode")) {
+                $(".error-msg-select").css("color", "#ff0000");
+                $(".select-error-glow").addClass("light-error");
+            } else {
+                $(".error-msg-select").css("color", "#ff5858");
+            }
+            $(".error-msg-select").text("Image already exists").show();
+            console.log("Image already exists for this email address");
+            setTimeout(function(){
+                $(".error-msg-select").fadeOut(500);
+            }, 3000);
+        } else {
+            assignImage($selectValue, $currentImg);
+            console.log($assignedPair);
+            var $selectedEmail = $("#select-email").val();
+            var $ulName = `.img-storage-${$selectedEmail.replace(/[@.]/g, "")}`;
+            if($("body").hasClass("lightmode")) {
+                $(".error-msg-select").css("color", "#259e01");
+            } else {
+                $(".error-msg-select").css("color", "#2fca00");
+            }
+            $(".error-msg-select").text("Image added!").show();
+            $($ulName).append(`<li><img src=${$currentImg} alt=${$selectValue}></li>`);
+            setTimeout(function(){
+                $(".error-msg-select").fadeOut(500);
+            }, 3000);
+        }
+    }
 });
 
 $(".select-error-glow").on("click", function(){
     $(this).removeClass("email-error-glow");
+    $(this).removeClass("light-error");
+});
+
+$("#select-email").on("change", function(){
+    var $selectedEmail = $("#select-email").val();
+    $(".assigned-image-box ul").not(`.img-storage-${$selectedEmail.replace(/[@.]/g, "")}`).hide();
+    $(`.img-storage-${$selectedEmail.replace(/[@.]/g, "")}`).show();
+});
+
+function iconToggle() {
+    const $btnIcon = $(".toggle-btn i");
+    if ($btnIcon.hasClass("fa-solid")) {
+        $btnIcon.removeClass("fa-solid").addClass("fa-regular");
+    } else {
+        $btnIcon.removeClass("fa-regular").addClass("fa-solid");
+    }
+}
+
+$(".toggle-btn").on('click', function() {
+    const $bodyAll = $("body");
+    iconToggle();
+    if ($bodyAll.hasClass("lightmode")) {
+        $bodyAll.removeClass("lightmode");
+        $(".base-box").removeClass("lightbox");
+        $(".email-textbox").removeClass("light-inputs");
+        $(".select-email").removeClass("light-inputs");
+    } else {
+        $("body").addClass("lightmode");
+        $(".base-box").addClass("lightbox");
+        $(".email-textbox").addClass("light-inputs");
+        $(".select-email").addClass("light-inputs");
+    }
+    if ($(this).hasClass("btnon")) {
+        $(this).removeClass("btnon");
+    } else {
+        $(this).addClass("btnon");
+    }
 });
